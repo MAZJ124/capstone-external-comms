49f07e0ed81dd8162fc75dafee221ab9492537e4
MAZJ124 <69350115+MAZJ124@users.noreply.github.com>
2024-02-21 12:28:20 +0800

    MQTT client subscribe to test message/publish to visualizer done

--- MQTTClient.py
+++ MQTTClient.py
@@ -8,7 +8,8 @@ class MQTTClient:
 
     def __init__(self):
         self.client = None
-        self.gamestate_topic = 'gamestate'
+        self.gamestate_topic = 'nus/capstone/group5/externalComms/mama/gamestate' # for public broker
+        # self.gamestate_topic = 'gamestate'
         self.hostname = None
         self.port = None 
         self.username = None 
@@ -31,14 +32,14 @@ class MQTTClient:
     def on_message(self, client, userdata, msg):
         print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-    def start_client(self):
+    def start_client(self, custom_on_message=None):
         # self.client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5, callback_api_version=paho.CallbackAPIVersion.VERSION2)
         self.client = paho.Client()
         # self.client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
         self.client.on_connect = self.on_connect
         self.client.on_publish = self.on_publish
         self.client.on_subscribe = self.on_subscribe
-        self.client.on_message = self.on_message
+        self.client.on_message = self.on_message if custom_on_message is None else custom_on_message
 
         # move to credentials file later on 
         username, password = 'capstone-visualizer', 'capstoneVisualizer'
@@ -51,9 +52,14 @@ class MQTTClient:
         self.hostname = hostname
 
         # self.client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-        self.client.tls_set(certifi.where())
-        self.client.username_pw_set(username, password)
-        self.client.connect(host=hostname, port=port)
+
+        # self.client.tls_set(certifi.where())
+        # self.client.username_pw_set(username, password)
+        # self.client.connect(host=hostname, port=port)
+
+        public_host = 'broker.hivemq.com'
+        public_port = 1883
+        self.client.connect(public_host, public_port)
 
     def subscribe_to_topic(self, topic):
         self.client.subscribe(topic, qos=0)
@@ -66,50 +72,3 @@ class MQTTClient:
         self.client.loop_stop()
         self.client.disconnect()
         self.is_subscribed = False
-
-    def publish_gamestate_to_viz_task(self, engine_to_viz):
-        try:
-            self.publish_client.publish_to_topic(self.gamestate_topic, 'testing')
-            print('Test MQTT publish to visualizer')
-        except Exception as e:
-            print(e)
-            print('MQTT test failed')
-        
-        while True:
-            try:
-                msg = engine_to_viz.get()
-                self.publish_to_topic(self.gamestate_topic, msg)
-            except Exception as e:
-                print(e)
-                print('Attempt MQTT re-publish')
-                try:
-                    self.close_client()
-                except:
-                    print('Failed to close. Starting new without closing.')
-                # self.publish_client = MQTTClient()
-                # self.publish_client.start_client()
-                time.sleep(5)
-            except:
-                break
-
-        self.close_client()
-        print('MQTT client closed')
-
-    def Mqtt_client_process_main(self, engine_to_viz):
-        self.start_client()
-        processes = []
-        # try:
-        publish_gamestate_process = Process(target=self.publish_gamestate_to_viz_task, args=(engine_to_viz,), daemon=True)
-        publish_gamestate_process.start()
-        # except Exception as e:
-        #     print('HELLO')
-        #     print(e)
-        
-        try:
-            processes.append(publish_gamestate_process)
-            for p in processes:
-                p.join()
-        except KeyboardInterrupt:
-            print('MQTT client process terminated')
-
-        return True
--- MQTTClientProcess.py
+++ MQTTClientProcess.py
@@ -8,7 +8,9 @@ class MqttClientProcess:
     def __init__(self):
         # self.publish_client = None
         # self.subscribe_client = None
-        self.gamestate_topic = 'gamestate'
+        self.gamestate_topic = 'nus/capstone/group5/externalComms/mama/gamestate' # for public broker
+        # self.gamestate_topic = 'gamestate'
+        self.in_range_topic = 'nus/capstone/group5/externalComms/mama/inrange'
         self.processes = []
 
     def publish_gamestate_to_viz_task(self, engine_to_viz):
@@ -42,17 +44,41 @@ class MqttClientProcess:
         publish_client.close_client()
         print('MQTT client closed')
 
+    def subscribe_from_viz_task(self):
+        subscribe_client = MQTTClient()
+        subscribe_client.start_client()
+        while True:
+            try:
+                subscribe_client.subscribe_to_topic(self.in_range_topic)
+            except Exception as e:
+                print(e)
+                print('Redo')
+                try:
+                    subscribe_client.close_client()
+                except:
+                    print('Failed to close old client')
+                subscribe_client = MQTTClient()
+                subscribe_client.start_client()
+                time.sleep(5)
+            except:
+                break
+        subscribe_client.close_client()
+        print('MQTT client closed')
+
     def Mqtt_client_process_main(self, engine_to_viz):
         # self.publish_client.start_client()
         try:
             publish_gamestate_process = Process(target=self.publish_gamestate_to_viz_task, args=(engine_to_viz,), daemon=True)
             publish_gamestate_process.start()
+            subscribe_inrange_process = Process(target=self.subscribe_from_viz_task, args=(), daemon=True)
+            subscribe_inrange_process.start()
         except Exception as e:
             print('HELLO')
             print(e)
         
         try:
             self.processes.append(publish_gamestate_process)
+            self.processes.append(subscribe_inrange_process)
             for p in self.processes:
                 p.join()
         except KeyboardInterrupt:
